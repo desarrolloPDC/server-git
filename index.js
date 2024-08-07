d94a4263155e8b3a09efc5c61fe221f3785ee820
FrancoG182 <francosebastiangonzalez100402@gmail.com>
2024-08-07 16:25:53 -0300

    puerto 4001

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express();
-const port =  4000;
+const port =  4001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
